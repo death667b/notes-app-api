93833f69fe2db1b96e896f10ce1d57b0c0b5053b
death667b <idaniel@spinnys.com>
2017-08-13 15:41:17 +1000

    Fix type in dynamodb-lib

--- libs/dynamodb-lib.js
+++ libs/dynamodb-lib.js
@@ -3,7 +3,7 @@ import AWS from 'aws-sdk';
 AWS.config.update({region:'ap-southeast-2'});
 
 export function call(action, params) {
-    const dynamoDb = new AWS.dynamoDB.DocumentClient();
+    const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
     return dynamoDb[action](params).promise();
 }
