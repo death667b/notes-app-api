19527cdc110ebe3f74c0f3e50a476d52505266b3
death667b <idaniel@spinnys.com>
2017-08-13 15:14:05 +1000

    working create.js

--- create.js
+++ create.js
@@ -2,9 +2,9 @@ import uuid from 'uuid';
 import AWS from 'aws-sdk';
 
 AWS.config.update({region:'ap-southeast-2'});
-const dynamoDb = new AWS.DynamoDb.DocumentClient();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
-export function mail(event, context, callback) {
+export function main(event, context, callback) {
     // Request body is passed in as a JSON encoded string in 'event.body'
     const data = JSON.parse(event.body);
 
