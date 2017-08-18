1d4fc1bedde195cb5271040da5c0719ff84aeb20
death667b <idaniel@spinnys.com>
2017-08-18 18:07:30 +1000

    Add catch-all route in Routes.js

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Home from './containers/Home';
+import NotFound from './components/NotFound';
 
 export default () => (
   <Switch>
     <Route path='/' exact component={Home} />
-  </Switch>
+    <Route component={NotFound} />
+  </ Switch>
 );
