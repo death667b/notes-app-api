90d1dfbd083095cb409516b079dad822ec40ff29
death667b <idaniel@spinnys.com>
2017-08-18 17:57:42 +1000

    Use RouteNavItem component instead of NavItem

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import {Link, withRouter} from 'react-router-dom';
-import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import {Navbar, Nav} from 'react-bootstrap';
 import Routes from './Routes';
+import RouteNavItem from './components/RouteNavItem';
 import './App.css';
 
 class App extends Component {
@@ -22,8 +23,8 @@ class App extends Component {
           </ Navbar.Header>
           <Navbar.Collapse>
             <Nav pullRight>
-              <NavItem onClick={this.handleNavLink} href='/signup'>Sign Up</ NavItem>
-              <NavItem onClick={this.handleNavLink} href='/login'>Login</ NavItem>
+              <RouteNavItem onClick={this.handleNavLink} href='/signup'>Sign Up</ RouteNavItem>
+              <RouteNavItem onClick={this.handleNavLink} href='/login'>Login</ RouteNavItem>
             </ Nav>
           </ Navbar.Collapse>
         </ Navbar>
