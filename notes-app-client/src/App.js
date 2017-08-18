637bbb1e7164a8e556dbac5a888e49d0600cbdff
death667b <idaniel@spinnys.com>
2017-08-18 17:26:09 +1000

    Add two new nav links to app.js

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
-import {Navbar} from 'react-bootstrap';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
 import Routes from './Routes';
 import './App.css';
 
 class App extends Component {
+  handleNavLink = () => {
+    alert('tt');
+  }
+
   render() {
     return (
       <div className="App container">
@@ -15,6 +19,12 @@ class App extends Component {
             </ Navbar.Brand>
             <Navbar.Toggle />
           </ Navbar.Header>
+          <Navbar.Collapse>
+            <Nav pullright>
+              <NavItem onClick={this.handleNavLink} href='/signup'>Sign Up</ NavItem>
+              <NavItem onClick={this.handleNavLink} href='/login'>Login</ NavItem>
+            </ Nav>
+          </ Navbar.Collapse>
         </ Navbar>
         <Routes />
       </div>
