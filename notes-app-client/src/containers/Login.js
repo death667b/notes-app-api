2f367cae3e9ae357d2946528a25f65d3aed056a7
death667b <idaniel@spinnys.com>
2017-08-18 18:20:05 +1000

    partial implementation of Login.js

--- notes-app-client/src/containers/Login.js
+++ notes-app-client/src/containers/Login.js
@@ -1,3 +1,36 @@
 import React, {Component} from 'react';
 import {Button, FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
 import './Login.css';
+
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      username: '',
+      password: '',
+    };
+  }
+
+  validateForm = () => {
+    return this.state.username.length > 0 && this.state.password.length > 0;
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div className='login'>
+        <form onSubmit={this.handleSubmit}>
+          <FormGroup controlId='username' bsSize='large'>
+            <ControlLabel>Email</ControlLabel>
+          </FormGroup>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
