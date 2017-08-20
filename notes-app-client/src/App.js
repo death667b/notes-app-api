c49491487daec9fae5625fb3450d71b2267d64fa
death667b <idaniel@spinnys.com>
2017-08-20 23:09:26 +1000

    Only render App.js when state.isLoadingUserToken is false

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -78,7 +78,7 @@ class App extends Component {
       updateUserToken: this.updateUserToken,      
     }
 
-    return (
+    return !this.state.isLoadingUserToken && (
       <div className="App container">
         <Navbar fluid collapseOnSelect>
           <Navbar.Header>
