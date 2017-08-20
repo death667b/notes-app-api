38711000243a8a9a37b6f95d6b38135333bd6c77
death667b <idaniel@spinnys.com>
2017-08-20 22:50:59 +1000

    Added methods to load the users Jwt from a broswer refresh

--- notes-app-client/src/App.js
+++ notes-app-client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {Link, withRouter} from 'react-router-dom';
 import {Navbar, NavItem, Nav} from 'react-bootstrap';
+import {CognitoUserPool} from 'amazon-cognito-identity-js';
+import config from './config';
 import Routes from './Routes';
 import RouteNavItem from './components/RouteNavItem';
 import './App.css';
@@ -12,6 +14,28 @@ class App extends Component {
     this.state = {
       userToken: null,
     }
+  }
+
+  getCurrentUser = () => {
+    const userPool = new CognitoUserPool({
+      UserPoolId: config.cognito.USER_POOL_ID,
+      ClientId: config.cognito.APP_CLIENT_ID,
+    });
+
+    return userPool.getCurrentUser();
+  }
+
+  getUserToken = (currentUser) => {
+    return new Promise( (resolve, reject) => {
+      currentUser.getSession( function(err, session) {
+        if (err) {
+          reject(err);
+          return;
+        }
+
+        resolve(session.getIdToken().getJwtToekn());
+      });
+    });
   }
 
   updateUserToken = (userToken) => {
