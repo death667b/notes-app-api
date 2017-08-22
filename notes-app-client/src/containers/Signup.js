8f56c33ac70b3cf89acf066a844f29c4357ede84
death667b <idaniel@spinnys.com>
2017-08-22 23:10:49 +1000

    Empty Class Signup with export

---
+++ notes-app-client/src/containers/Signup.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom';
+import {HelpBlock, FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
+import LoaderButton from '../components/LoaderButton';
+import './Signup.css';
+
+class Signup extends Component {
+    
+}
+
+export default withRouter(Signup);
