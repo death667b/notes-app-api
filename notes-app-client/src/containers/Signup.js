f0075c65d014e7dd592c60d2ac765bff6839aea6
death667b <idaniel@spinnys.com>
2017-08-22 23:20:18 +1000

    Helper functions written in Signup.js

--- notes-app-client/src/containers/Signup.js
+++ notes-app-client/src/containers/Signup.js
@@ -5,7 +5,48 @@ import LoaderButton from '../components/LoaderButton';
 import './Signup.css';
 
 class Signup extends Component {
-    
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: false,
+            username: '',
+            password: '',
+            confirmPassword: '',
+            confirmationCode: '',
+            newUser: null,
+        };
+    }
+
+    validateForm = () => {
+        return this.state.username.length > 0 
+            && this.state.password.length > 0
+            && this.state.password === this.state.confirmPassword;
+    }
+
+    validateConfirmationForm = () => {
+        return this.state.confirmationCode.length > 0;
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.id]: event.target.value,
+        });
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault();
+
+        this.setState({isLoading: true});
+        this.setState({newUser: 'test'});
+        this.setState({isLoading: false});
+    }
+
+    handleConfirmationSubmit = async (event) => {
+        event.preventDefault();
+
+        this.setState({isLoading: true});
+    }
 }
 
 export default withRouter(Signup);
