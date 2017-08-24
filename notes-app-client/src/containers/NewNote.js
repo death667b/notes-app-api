ef627a0874522a7f6a8f28c19f77f42501df052e
death667b <idaniel@spinnys.com>
2017-08-24 21:51:20 +1000

    Added imports and empty Class

---
+++ notes-app-client/src/containers/NewNote.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import withrouter from 'react-router-dom';
+import {FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
+import LoaderButton from '../components/LoaderButton';
+import config from '../config.js';
+import './NewNote.css';
+
+class NewNote extends Component {
+    
+}
+
+export default withrouter(NewNote);
