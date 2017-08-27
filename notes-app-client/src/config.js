6b030851bc08cce4896fb502bf4b3f90f60755d6
death667b <idaniel@spinnys.com>
2017-08-27 16:57:30 +1000

    Update config.js with apiGateway info

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -1,5 +1,9 @@
 export default {
     MAX_ATTACHMENT_SIZE: 5000000,
+    apiGateway: {
+        URL: 'https://ohzexixk93.execute-api.ap-southeast-2.amazonaws.com/prod',
+        REGION: 'ap-southeast-2',
+    },
     cognito: {
         USER_POOL_ID: 'ap-southeast-2_bvWqZeTFk',
         APP_CLIENT_ID: '5r1111n8f1dipd90nkgni81qos',
