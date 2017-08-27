d613aa2f3a629e650f65ccc8cda8e1453c7cf338
death667b <idaniel@spinnys.com>
2017-08-27 13:54:02 +1000

    Add region and identiy pool id to config.js

--- notes-app-client/src/config.js
+++ notes-app-client/src/config.js
@@ -3,5 +3,7 @@ export default {
     cognito: {
         USER_POOL_ID: 'ap-southeast-2_bvWqZeTFk',
         APP_CLIENT_ID: '5r1111n8f1dipd90nkgni81qos',
+        REGION: 'ap-southeast-2',
+        IDENTITY_POOL_ID: 'ap-southeast-2:42933e86-25aa-4dc8-8384-c084a4bf7877',
     }
 };
