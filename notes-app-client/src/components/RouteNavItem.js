d976beaff57345705ae3e94e8eedb44a082cbf3a
death667b <idaniel@spinnys.com>
2017-08-31 16:14:25 +1000

    <Broken Code> Put the onclick in RouteNavItem in the wrong place

--- notes-app-client/src/components/RouteNavItem.js
+++ notes-app-client/src/components/RouteNavItem.js
@@ -4,8 +4,8 @@ import {NavItem} from 'react-bootstrap';
 
 export default (props) => (
     <Route path={props.href} exact children={({ match, history }) => (
-        <NavItem {...props} active={ match ? true : false}>{props.children}
+        <NavItem {...props} active={ match ? true : false}
             onClick={e => history.push(e.currentTarget.getAttribute('href'))}
-        </ NavItem>
+        >{props.children}</ NavItem>
     )} />
 );
