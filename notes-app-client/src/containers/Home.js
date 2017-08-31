68cc62b9200bc6432b4307d9ee58c01100c44e0f
death667b <idaniel@spinnys.com>
2017-08-31 13:15:08 +1000

    Updated Home.js to display a mock note when auth'ed

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -1,17 +1,50 @@
 import React, {Component} from 'react';
+import {PageHeader, ListGroup} from 'react-bootstrap';
 import './Home.css';
 
 class Home extends Component {
-    render() {
-        return(
-          <div className='Home'>
-            <div className='lander'>
-              <h1>Scratch</h1>
-              <p>A simple note taking app</p>
-            </div>
-          </div>
-        );
-    }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoading: true,
+      notes: [],
+    };
+  }
+
+  renderNotesList = (notes) => {
+    return <div><p>Notes will go here..</p></div>;
+  }
+
+  renderLander = () => {
+    return (
+      <div className='home'>
+        <div className='lander'>
+          <h1>Scratch</h1>
+          <p>A simple note taking app</p>
+        </div>
+      </div>
+    );
+  }
+
+  renderNotes = () => {
+    return (
+      <div className='notes'>
+        <PageHeader>Your Notes</PageHeader>
+        <ListGroup>
+          {!this.state.isLoading && this.renderNotesList(this.state.notes)}
+        </ListGroup>
+      </div>
+    );
+  }
+
+  render() {
+      return(
+        <div className='Home'>
+          {this.props.isAuthenticated ? this.renderNotes() : this.renderLander()}
+        </div>
+      );
+  }
 }
 
 export default Home;
