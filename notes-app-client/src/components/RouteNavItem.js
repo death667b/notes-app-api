ffd75d0f699be612cfc4e90fa5650a2c105cfe5a
death667b <idaniel@spinnys.com>
2017-09-02 19:15:56 +1000

    <Broken Code> Moved the onClick to named handle

--- notes-app-client/src/components/RouteNavItem.js
+++ notes-app-client/src/components/RouteNavItem.js
@@ -2,10 +2,14 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 import {NavItem} from 'react-bootstrap';
 
+const handleClick = (history, event) => {
+    history.push(event.currentTarget.getAttribute("href"))
+}
+
 export default (props) => (
     <Route path={props.href} exact children={({ match, history }) => (
         <NavItem {...props} active={ match ? true : false}
-            onClick={e => history.push(e.currentTarget.getAttribute('href'))}
+            onClick={handleClick.bind(this, history)}
         >{props.children}</ NavItem>
     )} />
 );
