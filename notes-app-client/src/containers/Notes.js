33b3c87b195b81ca0ea2c21413a60574a34aa1fd
death667b <idaniel@spinnys.com>
2017-10-01 14:44:29 +1000

    Add form to a single note display

--- notes-app-client/src/containers/Notes.js
+++ notes-app-client/src/containers/Notes.js
@@ -1,5 +1,9 @@
 import React, {Component} from 'react';
 import {invokeApig} from '../libs/awsLib';
+import {FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
+import LoaderButton from '../components/LoaderButton';
+import config from '../config';
+import './Notes.css';
 
 export default class Notes extends Component {
     constructor(props) {
@@ -10,6 +14,8 @@ export default class Notes extends Component {
         this.state = {
             note: null,
             content: '',
+            isLoading: null,
+            isDeleting: null,
         }
     }
 
@@ -25,15 +31,93 @@ export default class Notes extends Component {
         }
     }
 
-    getNote() {
-        return invokeApig({ path: `/notes/${this.props.match.params.id}` });
+    getNote = () => invokeApig({ path: `/notes/${this.props.match.params.id}` });
+
+    validateForm = () => this.state.content.length > 0;
+
+    formatFilename = str => 
+        str.length < 50
+        ? str
+        : str.substr(0, 20) + '...' + str.substr(str.length - 20, str.length);
+
+    handleFileChange = event => this.file = event.target.files[0];
+
+    handleSubmit = async event => {
+        event.preventDefault();
+
+        if (this.file && this.file.size > config.MAX_ATTACHMENT_SIZE) {
+            alert('Please pick a file smaller than 5MB');
+            return;
+        }
+
+        this.setState({ isLoading: true });
+    }
+
+    handleDelete = async event => {
+        event.preventDefault();
+
+        const confirmed = window.confirm(
+            'Are you sure you want to delete this note?'
+        );
+
+        if (!confirmed) return;
+
+        this.setState({ isDeleting: true });
     }
 
     render() {
-        return (<div className='notes'>
-            <p>{this.props.match.params.id}</p>
-            <p>{this.state.content}</p>
+        return (
+            <div className='notes'>
+                {this.state.note && 
+                    <form onSubmit={this.handleSubmit}>
+                        <FormGroup controlId='content'>
+                            <FormControl 
+                                onChange={this.handleChange}
+                                value={this.state.content}
+                                componentClass='textarea'
+                            />
+                        </FormGroup>
+
+                        {this.state.note.attchment &&
+                            <FormGroup>
+                                <ControlLabel>Attachment</ControlLabel>
+                                <FormControl.static>
+                                    <a
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        href={this.state.note.attchment}
+                                    >
+                                        {this.formatFilename(this.state.note.attchment)}
+                                    </a>
+                                </FormControl.static>
+                            </ FormGroup>}
+
+                        <FormGroup controlId='file'>
+                            {!this.state.note.attchment &&
+                                <ControlLabel>Attchment</ControlLabel>}
+                            <FormControl onChange={this.handleFileChange} type='file' />
+                        </FormGroup>
+
+                        <LoaderButton
+                            block
+                            bsStyle='primary'
+                            bsSize='large'
+                            disabled={this.validateForm()}
+                            type='submit'
+                            isLoading={this.state.isLoading}
+                            text='Save'
+                            loadingText='...Saving'
+                        />
+                        <LoaderButton
+                            block
+                            bsStyle='danger'
+                            bsSize='large'
+                            isLoading={this.state.isDeleting}
+                            text='Delete'
+                            loadingText='...Deleting'
+                        />
+                    </form>}
             </div>
-            );
+        );
     }
 }
