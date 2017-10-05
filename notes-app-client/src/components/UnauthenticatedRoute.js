c63fc67cd383dad5829520dbd048bda7b89dba07
death667b <idaniel@spinnys.com>
2017-10-05 22:11:29 +1000

    Filter out the query string in the URI

--- notes-app-client/src/components/UnauthenticatedRoute.js
+++ notes-app-client/src/components/UnauthenticatedRoute.js
@@ -8,3 +8,15 @@ export default ({component: C, props: cProps, ...rest}) =>
             : <Redirect to = '/' />
         }
     />;
+
+const queryString = (name, url = window.location.href) => {
+    name = name.replace(/[[]]/g, "\\$&");
+
+    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)", "i");
+    const results = regex.exec(url);
+
+    if (!results) return null;
+    if (!results[2]) return '';
+
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
